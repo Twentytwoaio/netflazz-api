1bc3fcfe1d4b762d60ab43859a82c160420c6ae7
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2025-01-01 04:41:57 +0700

    Update cek-account.php

--- subscribe/cek-ewallet/cek-account.php
+++ subscribe/cek-ewallet/cek-account.php
@@ -5,7 +5,7 @@ $url = "https://api.nf22.my.id/subscribe/ewallet.php";
 // Data yang akan dikirimkan via POST
 $data = [
     'api_key' => 'xxxxxxxxxxxxxxxxxxxxx',  // Ganti dengan API Key Anda
-    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxx',          // Ganti dengan PIN Anda
+    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxx',          // Ganti dengan Secret Key Anda
     'action' => 'cek',        // Mengambil data cek akun
     'ewalletCode' => 'SHOPEEPAY',      // Ewallet Code yang ingin dicek
     'accountNumber' => 'No Ewallet'  // Nomor Akun yang ingin dicek
