9f54fb84663f3ab2bd73271d3f9b963adc88892e
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2025-01-01 04:41:16 +0700

    Update cek-account.php

--- subscribe/cek-rekening/cek-account.php
+++ subscribe/cek-rekening/cek-account.php
@@ -5,7 +5,7 @@ $url = "https://api.nf22.my.id/subscribe/rekening.php";
 // Data yang akan dikirimkan via POST
 $data = [
     'api_key' => 'xxxxxxxxxxxxxxxxxxxxx',  // Ganti dengan API Key Anda
-    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxx',          // Ganti dengan PIN Anda
+    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxx',          // Ganti dengan Secret Key Anda
     'action' => 'cek',        // Mengambil data cek akun
     'bankCode' => '270',      // Bank Code yang ingin dicek
     'accountNumber' => 'No Rekening'  // Nomor Akun yang ingin dicek
