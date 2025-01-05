02d4c2c3982c2591ea868333d14621f0358487b8
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2025-01-06 03:06:25 +0700

    Create cek-ongkir.php

---
+++ subscribe/cek-tarif/cek-ongkir.php
@@ -0,0 +1,40 @@
+<?php
+// URL API Anda
+$url = "https://api.nf22.my.id/subscribe/cek-tarif.php";
+
+// Data yang akan dikirimkan via POST
+$data = [
+    'api_key' => 'xxxx',      // Ganti dengan API Key Anda
+    'secret_key' => 'xx1234',  // Ganti dengan Secret Key Anda
+    'action' => 'cekOngkir',        // Action Jangan Diubah
+    'courier' => 'jne',            // Ubah Dengan Expedisi (jne/sicepat/anteraja/lion/sap/pos/ide)
+    'origin' => 'jakarta',            // nama kota/kab ambil dari cek wilayah ongkir (lokasi pengirim)
+    'destination' => 'cilegon',    // nama kota/kab ambil dari cek wilayah ongkir (lokasi penerima)
+    'weight' => '1',            // satuan kilo (jika 500gram gunakan 0.5)
+    'panjang' => '100',         // ubah untuk panjang barang
+    'lebar' => '100',           // ubah untuk lebar barang
+    'tinggi' => '100'           // ubah untuk tinggi barang
+];
+
+// Inisiasi cURL
+$ch = curl_init($url);
+
+// Set opsi cURL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+// Eksekusi cURL dan simpan respons
+$response = curl_exec($ch);
+
+// Cek apakah ada error saat mengambil data
+if($response === false) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    // Menampilkan respons API
+    echo $response;
+}
+
+// Tutup koneksi cURL
+curl_close($ch);
+?>
