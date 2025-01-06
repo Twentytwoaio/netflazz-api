f5792a700149900aa520baaa42d4f1e63c280c5b
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2025-01-07 00:25:16 +0700

    Create get-kota.php

---
+++ subscribe/solat/get-kota.php
@@ -0,0 +1,34 @@
+<?php
+// URL API Anda
+$url = "https://api.nf22.my.id/subscribe/solat.php";
+
+// Data yang akan dikirimkan via POST
+$data = [
+    'api_key' => 'xxxxxxxxxxxxxxxxxxxxx',      // Ganti dengan API Key Anda
+    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxx',  // Ganti dengan Secret Key Anda
+    'action' => 'listKota',        // Action Jangan Diubah
+    'kota' => 'jakarta'   // Ubah dengan nama Kota
+];
+
+// Inisiasi cURL
+$ch = curl_init($url);
+
+// Set opsi cURL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+// Eksekusi cURL dan simpan respons
+$response = curl_exec($ch);
+
+// Cek apakah ada error saat mengambil data
+if($response === false) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    // Menampilkan respons API
+    echo $response;
+}
+
+// Tutup koneksi cURL
+curl_close($ch);
+?>
