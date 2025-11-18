0043f867bdf2d67594f966e0087a7b532c554a90
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2025-11-19 00:13:06 +0700

    Create get-operator.php

---
+++ transaksi/pulsa-ppob/get-operator.php
@@ -0,0 +1,45 @@
+<?php
+
+// API endpoint dan parameter
+$api_url = 'https://api.nf22.my.id/prabayar';
+$api_key = 'xxxxxxxxxxxxxxxxxxxxx'; // Ganti dengan api key anda
+$pin = '1234'; // Ganti dengan pin anda
+$action = 'operator';
+
+// Data yang akan dikirim sebagai payload
+$postdata = [
+    'api_key' => $api_key,
+    'pin' => $pin,
+    'action' => $action,
+];
+
+// Inisialisasi cURL
+$ch = curl_init();
+
+// Setel opsi cURL
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postdata));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Eksekusi cURL dan dapatkan respon
+$response = curl_exec($ch);
+
+// Tutup koneksi cURL
+curl_close($ch);
+
+// Dekode respon JSON
+$json_result = json_decode($response, true);
+
+// Tampilkan hasil
+if ($json_result['status'] == true) {
+    $layanan = $json_result['data'];
+    
+    foreach ($layanan as $service) {
+        echo "Operator: " . $service['operator'] . "<br><br>";
+    }
+} else {
+    echo "Pengambilan layanan gagal. Pesan: " . $json_result['data']['pesan'];
+}
+
+?>
