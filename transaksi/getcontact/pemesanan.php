8664019e4b936516d1b958b1cd2de5c94d3efb1b
Twentytwo Aio <83259556+Twentytwoaio@users.noreply.github.com>
2026-09-17 03:03:30 +0700

    Update pemesanan.php

--- transaksi/getcontact/pemesanan.php
+++ transaksi/getcontact/pemesanan.php
@@ -1,19 +1,18 @@
 <?php
 
 // API endpoint dan parameter
-$api_url = 'https://api.nf22.my.id/prabayar';
+$api_url = 'https://api.nf22.my.id/getcontact';
 $api_key = 'xxxxxxxxxxxxxxxxxxxxx'; // Ganti dengan api key anda
 $pin = '1234'; // Ganti dengan pin anda
 $action = 'pemesanan';
-$nomor = '08123456789'; // Ganti dengan nomor HP atau tujuan yang ingin dilakukan pengecekan
+$nomor = '08123456789'; // Ganti dengan nomor HP yang ingin dicek
 
 // Data yang akan dikirim sebagai payload
 $postdata = [
     'api_key' => $api_key,
     'pin' => $pin,
     'action' => $action,
-    'layanan' => $layanan,
-    'target' => $target,
+    'nomor' => $nomor,
 ];
 
 // Inisialisasi cURL
@@ -36,9 +35,18 @@ $json_result = json_decode($response, true);
 
 // Tampilkan hasil
 if ($json_result['status'] == true) {
-    echo "Pemesanan berhasil. ID Pesanan: " . $json_result['data']['id'];
+    echo "Order ID: " . $json_result['data']['id'] . "<br>";
+    echo "Nomor: " . $json_result['data']['target'] . "<br>";
+    echo "Nama Terdaftar: " . $json_result['data']['nama'] . "<br>";
+    echo "Operator: " . $json_result['data']['operator'] . "<br>";
+    echo "Foto Profil: " . $json_result['data']['profil'] . "<br>";
+    echo "Spam: " . $json_result['data']['spam'] . "<br>";
+    echo "Mencurigakan: " . $json_result['data']['mencurigakan'] . "<br>";
+    echo "Total Tag: " . $json_result['data']['total_tag'] . "<br>";
+    echo "Harga: Rp " . number_format($json_result['data']['harga'], 0, ',', '.') . "<br>";
+    echo "Daftar Tag: " . implode(', ', $json_result['data']['tags']) . "<br>";
 } else {
-    echo "Pemesanan gagal. Pesan: " . $json_result['data']['pesan'];
+    echo "Pengecekan gagal. Pesan: " . $json_result['data']['pesan'];
 }
 
 ?>
